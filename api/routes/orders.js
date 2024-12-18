9934db759ce426232ac44edc4a17391b4addbebe
Zoe-life <merlyn@gmail.com>
2024-12-18 19:04:29 +0300

    Handling errors

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -9,8 +9,13 @@ router.get('/', (res, req, next) => {
 });
 
 router.post('/', (res, req, next) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    };
     res.status(201).json({
-        message: 'Order created'
+        message: 'Order created',
+        order: order
     });
 });
 
