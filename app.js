7bc6f200efd3cacf1a30cbeaf9d3ac253ccada09
Zoe-life <merlyn@gmail.com>
2024-12-24 11:17:35 +0300

    Fix: Error handling

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://rest-shop:' + process.env.MONGO_ATLAS_PW + '@cluster0.lifak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'API is running'
+    });
+});
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
@@ -14,24 +24,25 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
-    if (req.method == 'OPTIONS') {
+    if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE');
         return res.status(200).json({});
     }
+    next();
 });
 
 //Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
-app.use((re, req, next) => {
+app.use((req, res, next) => {
     const error = new Error('Not found');
     error.status = 404;
     next(error);
 });
 
 app.use((error, req, res, next) => {
-    res.status(error.status) || 500;
+    res.status(error.status || 500);
     res.json({
         error: {
             message: error.message
