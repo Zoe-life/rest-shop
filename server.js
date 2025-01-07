ea0d37b3f336d5818d8ae47c8e4b01bdf6231c02
Zoe-life <merlyn@gmail.com>
2025-01-08 02:10:55 +0300

    Fix: fixed mongoose conflict

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const http = require('http');
 const app = require('./app');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3001;
 
 const server = http.createServer(app);
 
-mongoose.connect(process.env.MONGODB_URI);
+//mongoose.connect(process.env.MONGODB_URI);
 
-server.listen(port);
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
