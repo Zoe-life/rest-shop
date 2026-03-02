505dc085000a13e87fdab5e9d48cca55f73d6dfc
Zoe-life <merlynzaw@gmail.com>
2026-03-02 17:38:57 +0300

    added crdentials=true to axios.ts

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -10,6 +10,7 @@ const API_URL = import.meta.env.VITE_BACKEND_URL ||
 export const api = axios.create({
   baseURL: API_URL,
   timeout: 10000,
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
